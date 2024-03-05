dcbf89fbdef221b647e5b0897a415628742f0c2b
cero-dev <cero-dev@users.noreply.github.com>
2024-03-05 14:57:07 -0600

    Add data validation for the put and post routes

--- server/routes.js
+++ server/routes.js
@@ -23,6 +23,20 @@ router.post("/comics", async (req, res) => {
     const {series, issue, format, hasRead} = req.body;
     const newComic = await collection.insertOne({series, issue, format, hasRead});
 
+    if(!series){
+        return res.status(400).json({msg: "Error: Missing Data"})
+    }else if(!issue){
+        return res.status(400).json({msg: "Error: Missing Data"})
+    }else if(!format){
+        return res.status(400).json({msg: "Error: Missing Data"})
+    }else if(!hasRead){
+        return res.status(400).json({msg: "Error: Missing Data"})
+    }
+
+    if(typeof hasRead !== "boolean"){
+        return res.status(400).json({msg: "invalid status"});
+    }
+
     res.status(200).json({series, issue, format, hasRead, _id: newComic.insertedId });
 })
 // delete /comics/:id
@@ -38,6 +52,20 @@ router.put("/comics/:id", async (req, res) => {
     const collection = getCollection();
     const _id = new ObjectId(req.params.id);
     const {series, issue, format, hasRead} = req.body
+
+    if(!series){
+        return res.status(400).json({msg: "Error: Missing Data"})
+    }else if(!issue){
+        return res.status(400).json({msg: "Error: Missing Data"})
+    }else if(!format){
+        return res.status(400).json({msg: "Error: Missing Data"})
+    }else if(!hasRead){
+        return res.status(400).json({msg: "Error: Missing Data"})
+    }
+
+    if(typeof hasRead !== "boolean"){
+        return res.status(400).json({msg: "invalid status"});
+    }
 
     const updatedComic = await collection.updateOne({_id}, {$set: {series, issue, format, hasRead}})
 
