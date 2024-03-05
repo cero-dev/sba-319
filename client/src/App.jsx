06d1240abbd844141280a1035e07748e16d9bd3f
cero-dev <cero-dev@users.noreply.github.com>
2024-03-05 12:49:25 -0600

    Connect backend to front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState} from "react";
+
 export default function App() {
+  const [message, setMessage] = useState("")
+
+  useEffect(()=>{
+    async function getComics(){
+      const res = await fetch("/api/comics")
+      const comics = await res.json();
+
+      setMessage(comics.msg);
+    }
+
+    getComics();
+  },[])
+
   return (
     <div>
       <main className="container">
         <h1>Comic Book Collection</h1>
+        {message && <p>{message}</p>}
       </main>
     </div>
   );
