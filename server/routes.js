b2e507ce23c3f435f9c2aa737883ee28f57915c8
cero-dev <cero-dev@users.noreply.github.com>
2024-03-05 12:42:16 -0600

    Fix typo

--- server/routes.js
+++ server/routes.js
@@ -5,19 +5,19 @@ const router = express.Router();
 
 // get /comics
 router.get("/comics", (req, res) => {
-    res.status(200).json({ msg: "GET REQUEST TO /api/todos" });
+    res.status(200).json({ msg: "GET REQUEST TO /api/comics" });
 })
 // post /comics
 router.post("/comics", (req, res) => {
-    res.status(200).json({ msg: "POST REQUEST TO /api/todos" });
+    res.status(200).json({ msg: "POST REQUEST TO /api/comics" });
 })
 // delete /comics/:id
 router.delete("/comics:id", (req, res) => {
-    res.status(200).json({ msg: "DELETE REQUEST TO /api/todos:id" });
+    res.status(200).json({ msg: "DELETE REQUEST TO /api/comics:id" });
 })
 //put /comics/:id
 router.put("/comics:id", (req, res) => {
-    res.status(200).json({ msg: "PUT REQUEST TO /api/todos:id" });
+    res.status(200).json({ msg: "PUT REQUEST TO /api/comics:id" });
 })
 
 module.exports = router;
